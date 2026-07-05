5cfab403543f44cd329bc6ef986e311833aafb8e
shreyat2124-lgtm <shreya.t2124@gmail.com>
2026-07-05 23:27:34 +0530

    Time: 0 ms (100%), Space: 10.9 MB (33.74%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -9,6 +9,64 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ class Solution {
+public:
+
+    vector<int> inorderTraversal(TreeNode* root)
+    {
+        vector<int> ans;
+
+        TreeNode* curr = root;
+
+        while(curr != NULL)
+        {
+            // If there is no left child,
+            // process current node and move right
+            if(curr->left == NULL)
+            {
+                ans.push_back(curr->val);
+
+                curr = curr->right;
+            }
+            else
+            {
+                // Find inorder predecessor
+                TreeNode* prev = curr->left;
+
+                while(prev->right != NULL &&
+                      prev->right != curr)
+                {
+                    prev = prev->right;
+                }
+
+                // Thread not created yet
+                if(prev->right == NULL)
+                {
+                    // Create temporary connection
+                    prev->right = curr;
+
+                    curr = curr->left;
+                }
+                else
+                {
+                    // Thread already exists
+
+                    // Remove it
+                    prev->right = NULL;
+
+                    // Process current node
+                    ans.push_back(curr->val);
+
+                    curr = curr->right;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+
+/*
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
@@ -34,4 +92,4 @@ public:
         
         
     }
-};
+};*/
