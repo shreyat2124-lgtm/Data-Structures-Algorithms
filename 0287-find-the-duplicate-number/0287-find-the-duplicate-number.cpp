74a5c123ada247cf4d3449aac637101ca0fa16a6
shreyat2124-lgtm <shreya.t2124@gmail.com>
2026-07-06 04:39:51 +0530

    Time: 0 ms (100%), Space: 65 MB (69.73%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,26 +1,41 @@
 class Solution {
 public:
-    int findDuplicate(vector<int>& nums) {
+
+    int findDuplicate(vector<int>& nums)
+    {
+        // STEP 1
+        // Find a meeting point inside the cycle
 
         int slow = nums[0];
         int fast = nums[0];
 
         do
         {
+            // Move one step
             slow = nums[slow];
+
+            // Move two steps
             fast = nums[nums[fast]];
-        }
-        while(slow != fast);
+
+        }while(slow != fast);
+
+        // STEP 2
+        // Start one pointer from the beginning
 
         slow = nums[0];
+
+        // Move both one step at a time
 
         while(slow != fast)
         {
             slow = nums[slow];
+
             fast = nums[fast];
         }
+
+        // Both pointers meet
+        // at the duplicate number
 
         return slow;
     }
 };
-    
