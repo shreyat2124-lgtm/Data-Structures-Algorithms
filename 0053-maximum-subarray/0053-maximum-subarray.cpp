22b4c97ab0f491ace109e5cb846bfbf692d0eaac
shreyat2124-lgtm <shreya.t2124@gmail.com>
2026-07-05 13:26:19 +0530

    Time: 3 ms (19.18%), Space: 71.8 MB (14.49%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+        int sum=0;
+        int maxi=nums[0];
+
+        for(int i=0;i<nums.size();i++)
+        {
+            sum+=nums[i];
+
+            maxi=max(maxi,sum);
+
+            if(sum<0)
+            sum=0;
+        }
+
+        return maxi;
+    }
+    
+};
+    
+//Carry positive sum forward.
+//Drop negative sum.
+//Keep track of maximum.
