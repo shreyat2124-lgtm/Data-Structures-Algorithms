00927117f5abd20c30200435eb56cd62ba179ad5
shreyat2124-lgtm <shreya.t2124@gmail.com>
2026-07-05 23:25:02 +0530

    Time: 0 ms (100%), Space: 10.8 MB (94.36%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -9,6 +9,57 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+class Solution {
+public:
+
+    vector<int> preorderTraversal(TreeNode* root)
+    {
+        vector<int> ans;
+
+        TreeNode* curr = root;
+
+        while(curr != NULL)
+        {
+            if(curr->left == NULL)
+            {
+                ans.push_back(curr->val);
+
+                curr = curr->right;
+            }
+            else
+            {
+                TreeNode* prev = curr->left;
+
+                while(prev->right != NULL &&
+                      prev->right != curr)
+                {
+                    prev = prev->right;
+                }
+
+                if(prev->right == NULL)
+                {
+                    // Process before going left
+                    ans.push_back(curr->val);
+
+                    prev->right = curr;
+
+                    curr = curr->left;
+                }
+                else
+                {
+                    prev->right = NULL;
+
+                    curr = curr->right;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+
+/*
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
@@ -33,4 +84,4 @@ public:
         preorder(root->right, ans);
     }
 
-};
+};*/
