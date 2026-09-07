e37b0b46a42774e5ab3fd04aa850de914eb84135
shreyat2124-lgtm <shreya.t2124@gmail.com>
2026-09-07 18:13:19 +0530

    Time: 88 ms (25.63%), Space: 89 MB (24.91%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -6,28 +6,20 @@ public:
         // Store every number.
         // HashSet gives O(1) lookup.
         unordered_set<int> st;
-
         for(int x : nums)
-        {
             st.insert(x);
-        }
-
+        
         int longest = 0;
-
         // Check every unique number.
         for(int x : st)
         {
-            // Start counting only if
-            // this number is the beginning
-            // of a sequence.
+            // Start counting only if this number is the beginning of a sequence.
             if(st.find(x-1) == st.end())
             {
                 int curr = x;
                 int len = 1;
 
-                // Keep moving forward
-                // while next consecutive
-                // number exists.
+                // Keep moving forward while next consecutive number exists.
                 while(st.find(curr+1) != st.end())
                 {
                     curr++;
