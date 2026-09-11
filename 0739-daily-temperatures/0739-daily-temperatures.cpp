4d32fbc79de664f5cf8700715ce06fb75358690c
shreyat2124-lgtm <shreya.t2124@gmail.com>
2026-09-11 14:48:30 +0530

    Time: 19 ms (70.98%), Space: 107.4 MB (35.38%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        
+vector<int> ans(temperatures.size(), 0);
+stack<int> st;
+
+for(int i = 0; i < temperatures.size(); i++) {
+
+    while(!st.empty() && temperatures[i] > temperatures[st.top()]) {
+         int j = st.top();
+         st.pop();
+
+        ans[j]=i-j;
+        
+    }
+
+    st.push(i);
+}
+return ans;
+    }
+};
